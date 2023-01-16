6b643004bd75844e13ddc2cadda58f0152489d29
Alisson <alissonhmb@gmail.com>
2023-01-16 19:39:50 -0300

    Add dropdown

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-//= require jquery
+require('jquery')
+//= require jquery_ujs
+//= require popper
+//= require rails-ujs
 //= require bootstrap-sprockets
 
 
